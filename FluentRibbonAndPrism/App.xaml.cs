fe49845f9accbdbace10e1099432586302c9f08f
Valeriu Caraulean <caraulean@gmail.com>
2010-06-13 15:27:24 +0200

    Bootstrapper working, the Shell is shown

--- FluentRibbonAndPrism/App.xaml.cs
+++ FluentRibbonAndPrism/App.xaml.cs
@@ -12,5 +12,10 @@ namespace FluentRibbonAndPrism
 	/// </summary>
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+			new Bootstrapper().Run();
+		}
 	}
 }
